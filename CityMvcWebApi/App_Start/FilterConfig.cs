927ad2c15cc5b0e9188ffc07e9962fa5ab45b76f
liel itzchak <liel.izcha321@gmail.com>
2022-01-06 11:38:18 +0200

    Add project files.

---
+++ CityMvcWebApi/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace CityMvcWebApi
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
